ded8c58166b594e9c765ce8536ce38528a19481c
MISSIC17 <prajjwalt9@gmail.com>
2024-06-12 16:18:35 +0545

    feat: alarm for battery low

--- scripts/.local/scripts/alarmWhenLowMem.sh
+++ scripts/.local/scripts/alarmWhenLowMem.sh
@@ -2,26 +2,12 @@
 
 #!/bin/bash
 
-#cutoff_frac is basically how much used memory can be at in terms of how much
-#total memory you have...2 is 1/2 of 100% or an alert when you're using 50% mem, etc.
-cutoff_frac=2
-
-total_mem=$(free|awk '/^Mem:/{print $2}')
-let "threshold = $total_mem / $cutoff_frac"
-
-while :
-do
-
-    free_mem=$(free|awk '/^Mem:/{print $4}')
-
-    if [ $free_mem -lt $threshold ]
-        then
-        notify-send "Low memory!!"
-    fi
-
-    sleep 5
-
+while true; do
+	bat_lvl=$(cat /sys/class/power_supply/BAT1/capacity)
+	if [ "$bat_lvl" -le 15 ]; then
+		notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
+		sleep 1200
+	else
+		sleep 120
+	fi
 done
-
-exit
-
