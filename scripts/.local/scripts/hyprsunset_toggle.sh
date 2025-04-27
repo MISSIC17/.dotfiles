219c9b2490f88d6d974c175042bb1d0d9f91b77f
MISSIC17 <prajjwalt9@gmail.com>
2025-04-28 05:14:07 +0545

    feat: add sunset toggle

---
+++ scripts/.local/scripts/hyprsunset_toggle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+STATE_FILE="/tmp/hyprsunset_state"
+
+if [ ! -f "$STATE_FILE" ]; then
+  echo "off" >"$STATE_FILE"
+fi
+
+STATE=$(cat "$STATE_FILE")
+if [ "$STATE" = "off" ]; then
+  hyprctl hyprsunset gamma 90
+  hyprctl hyprsunset temperature 4500
+  echo "on" >"$STATE_FILE"
+else
+  hyprctl hyprsunset gamma 100
+  hyprctl hyprsunset temperature 4000
+  echo "off" >"$STATE_FILE"
+fi
